4105bd9d464cb9b9acaabcfc3fb091fb858e4b6b
Andrew Walton <163466371+AndrewASUS@users.noreply.github.com>
2025-09-14 12:37:16 +1000

    Installed/configured inngest, clerk

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,9 +1,15 @@
-import dotenv from "dotenv"
-
+import "dotenv/config"
 
 
 export const ENV = {
   PORT: process.env.PORT || 5001,
   MONGO_URI: process.env.MONGO_URI,
-  NODE_ENV: process.env.NODE_ENV
+  NODE_ENV: process.env.NODE_ENV,
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  STREAM_API_API: process.env.STREAM_API_API,
+  STREAM_API_SECRET: process.env.STREAM_API_SECRET,
+  SENTRY_DSN: process.env.SENTRY_DSN,
+  INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+  INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,21 @@
 import express from "express"
 import { ENV } from "./config/env.js"
+import { connectDB } from "./config/db.js"
+import { clerkMiddleware } from "@clerk/express"
+import { functions, inngest } from "./config/inngest.js"
+import { serve } from "inngest/express"
 
 
 const app = express()
 
+app.use(clerkMiddleware()) // Can't check if user is authenticated without this (req.auth)
 
-console.log("Mongo uri", ENV.MONGO_URI)
+app.use(express.json()) // Allows req.body JSON data 
+
+
+
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
 app.get("/", (req, res) => {
@@ -13,6 +23,21 @@ app.get("/", (req, res) => {
 })
 
 
-app.listen(ENV.PORT, () => {
-  console.log("Server is running on PORT :" + ENV.PORT )
-})
+const startServer = async () => {
+  try {
+    await connectDB()
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log("Server running on PORT: " + ENV.PORT)
+      })
+    }
+  } catch (error) {
+    console.error("Error startig server", error)
+    process.exit(1)
+  }
+}
+
+
+startServer()
+
+export default app
